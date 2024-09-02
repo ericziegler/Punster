720b64a87954339716ce1ab9b5272f1c80f10be3
Eric Ziegler <eric@zigamajig.com>
2024-09-01 22:10:49 -0400

    EZ: Initial commit.

---
+++ Punster/PunsterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PunsterApp.swift
+//  Punster
+//
+//  Created by Eric Ziegler on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct PunsterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
